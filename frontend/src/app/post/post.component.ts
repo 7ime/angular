84de29a082582f079e938de749105d66c97c9bb5
7ime <lopatsin1990@gmail.com>
2020-01-18 10:22:05 +0300

    ngFor ngIf ngClass

---
+++ frontend/src/app/post/post.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
+})
+export class PostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  list = [
+    {
+      name: '1',
+      desc: 'hello world'
+    },
+    {
+      name: '2'
+    }
+  ]
+}
